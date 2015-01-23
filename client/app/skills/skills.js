c5900b3bc2f332fbc512ca922383b0ad3d97badc
Bryan L. Venable <bryanlvenable@gmail.com>
2015-01-22 19:59:32 -0800

    added skills module

---
+++ client/app/skills/skills.js
@@ -0,0 +1,14 @@
+/*
+Note: This is a template copied from main
+*/
+'use strict';
+
+angular.module('tikrApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
