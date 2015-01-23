69afc21b6f9de8195dfcf973150208b90ab96ebc
Richard VanBreemen <rvbsanjose@me.com>
2015-01-22 16:38:47 -0800

    Added in the index.js file for the message feature

---
+++ server/api/message/index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var express = require('express');
+var controller = require('./message.controller');
+var config = require('../../config/environment');
+var auth = require('../../auth/auth.service');
+
+var router = express.Router();
+
+// GET methods
+
+router.get('/', auth.hasRole('admin'), controller.index);
+router.get('/me', auth.isAuthenticated(), controller.me);
+router.get('/:id', auth.isAuthenticated(), controller.show);
+
+// POST methods
+
+router.post('/', controller.create);
+
+
+// DELETE methods
+
+router.delete('/:id', auth.hasRole('admin'), controller.destroy);
+
+module.exports = router;
