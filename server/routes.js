e5aaf9e4274d911fcc241671fced89b6fc578cad
Richard VanBreemen <rvbsanjose@me.com>
2015-01-22 16:31:03 -0800

    Required the wrong file for the messages route

--- server/routes.js
+++ server/routes.js
@@ -11,7 +11,7 @@ module.exports = function(app) {
   // Insert routes below
   app.use('/api/things', require('./api/thing'));
   app.use('/api/users', require('./api/user'));
-  app.use('/api/messages', require('./api/user'));
+  app.use('/api/messages', require('./api/message'));
 
   app.use('/auth', require('./auth'));
 
