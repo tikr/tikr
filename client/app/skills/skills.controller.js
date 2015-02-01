36e95e41df874561233cc6391e21d6b2d0a18f5e
Bryan L. Venable <bryanlvenable@gmail.com>
2015-01-31 16:59:52 -0800

    Auth not defined

--- client/app/skills/skills.controller.js
+++ client/app/skills/skills.controller.js
@@ -11,12 +11,21 @@ angular.module('tikrApp')
       $scope.awesomeThings = awesomeThings;
     });
 
+    $scope.isCollapsed = true;
+    $scope.isLoggedIn = Auth.isLoggedIn;
+    $scope.isAdmin = Auth.isAdmin;
+    $scope.getCurrentUser = Auth.getCurrentUser;
+
     $scope.addThing = function() {
       if($scope.newThing === '') {
         return;
       }
       $http.post('/api/things', { name: $scope.newThing });
       $scope.newThing = '';
+    };
+
+    $scope.loginOauth = function(provider) {
+      $window.location.href = '/auth/' + provider;
     };
 
     $scope.deleteThing = function(thing) {
